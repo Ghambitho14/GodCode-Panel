4ce8f5dd703b0e2edee6dd16aa137127889563f6
Ghambitho14 <bel4ndria.d.jhon@gmail.com>
2026-03-30 22:25:45 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import { PwaServiceWorkerRegister } from "../components/PwaServiceWorkerRegister";
 
@@ -65,6 +66,7 @@ export default function RootLayout({
 			>
 				<PwaServiceWorkerRegister />
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
